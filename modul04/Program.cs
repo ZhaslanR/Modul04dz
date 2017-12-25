b0558ddafb1f523aa5e624e91f1bbb20ce656e5a
ZhaslanR <zhaslan_12@bk.ru>
2017-12-25 18:04:42 +0600

    Ааа

--- modul04/Program.cs
+++ modul04/Program.cs
@@ -10,7 +10,21 @@ namespace modul04
     {
         static void Main(string[] args)
         {
-
+            int denomination;
+            int countDenomination;
+            int n;
+            Money money = new Money();
+            Console.WriteLine("У вас купюры какого наминала:");
+            denomination = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколька купюр такого наминала:");
+            countDenomination = int.Parse(Console.ReadLine());
+            money.Denomination = denomination;
+            money.CountDenomination = countDenomination;
+            money.Show();
+            Console.WriteLine("Сколько стоит товар который вы хотите купить:");
+            n = int.Parse(Console.ReadLine());
+            money.Buying(n);
+            money.FullBuying(n);
         }
     }
 }
